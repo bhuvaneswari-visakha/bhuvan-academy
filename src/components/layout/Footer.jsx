da5bb46e6dc598f0ebf3734ed5f2a9e60b8a8016
bhuvaneswari-visakha <bhuvaneswarivishaka@gmail.com>
2026-07-27 17:29:23 +0530

    Update Footer section

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -20,15 +20,6 @@ const programs = [
 
 const socialLinks = [
   {
-    label: "Facebook",
-    href: "#",
-    icon: (
-      <svg width="16" height="16" viewBox="0 0 24 24" fill="currentColor">
-        <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z" />
-      </svg>
-    ),
-  },
-  {
     label: "WhatsApp",
     href: "https://wa.me/919396888883",
     icon: (
